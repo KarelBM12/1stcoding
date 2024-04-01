e24961714e3759e8bcf5da502aa3ada3ab9eb89d
KarelBM12 <165615816+KarelBM12@users.noreply.github.com>
2024-03-31 19:54:23 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace LoggingCode
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string username;
+            string password;
+            int attempts = 3;
+
+            while (attempts > 0)
+            {
+
+                {
+                   
+                    Console.Write("Enter username: ");
+                    username = Console.ReadLine();
+                    Console.Write("Enter password: ");
+                    password = Console.ReadLine();
+
+                    if (username == "Admin" && password == "Admin1234")
+                    {
+                        Console.WriteLine("Logging in successful");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input");
+                        attempts--;
+
+                        Console.WriteLine("Remaining attempts:" + attempts + "\n");
+                    }
+                }
+                if (attempts == 0)
+                {
+                    Console.WriteLine("Maximum attempts reached. Try again later");
+                }
+            }
+        }
+    }
+}
+
+
+                    
